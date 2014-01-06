47ccb4e9c149ca73104f2edda30a9d328c03129b
Pumpkin Lee <wait4pumpkin@gmail.com>
2014-01-06 21:29:06 +0800

    Initial Commit

---
+++ Patch-Based-Background-Model/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Patch-Based-Background-Model
+//
+//  Created by Pumpkin Lee on 1/6/14.
+//  Copyright (c) 2014 Pumpkin Lee. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
